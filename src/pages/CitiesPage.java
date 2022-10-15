efba5bf474fe62c07f6d241601cf8fa289c43176
Selena Radovanovic <selena.milosevic@gmail.com>
2022-10-15 13:05:54 +0200

    MessagePopUpPage update

--- src/pages/CitiesPage.java
+++ src/pages/CitiesPage.java
@@ -1,5 +1,93 @@
 package pages;
 
-public class CitiesPage {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CitiesPage {
+//	dugme New Item
+//	input za pretragu
+//	metodu koja ceka da se dijalog za editovanje i kreiranje pojavi
+//	metodu koja ceka da se dijalog za brisanje pojavi
+//	save dugme iz dijalog za editovanje i kreiranje
+//	delete dugme iz dijaloga za brisanje
+//	metodu koja ceka da se u tabeli pojavi odredjeni broj redova. 
+//	Metoda kao parametar prima broj redova. Ako se metoda pozove sa 
+//	parametrom 5, to znaci da metoda ceka da se u tabeli pojavi 5 redova.
+//	metodu koja vraca celiju iz odredjenog reda. Metoda kao parametre prima 
+//	red i kolonu a vraca td iz tabele. Npr: ukoliko se metoda pozove row=2, 
+//	cell=3 tada metoda vraca trecu celiju iz 2 reda.
+//	metodu koja vraca edit dugme trazenog reda. Metoda kao parametar prima 
+//	red a vraca dugme. Npr ako se metoda pozove row=3, metoda vraca edit dugme 
+//	iz treceg reda tabele
+//	metodu koja vraca delete dugme trazenog reda. Metoda kao parametar prima 
+//	red a vraca dugme. Npr ako se metoda pozove row=3, metoda vraca delete 
+//	dugme iz treceg reda tabele
+
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	public CitiesPage(WebDriver driver, WebDriverWait wait) {
+		this.driver = driver;
+		this.wait = wait;
+	}
+
+	//New Item Button
+	public WebElement getNewItemBtn() {
+		return (WebElement) driver.findElements(By.className("btnNewItem"));
+	}
+	
+	//Input City Name Dlg
+	public WebElement getInputNewItemName() {
+		return (WebElement) driver.findElements(By.id("name"));
+	}
+
+	//Search Input
+	public WebElement getSearchInput() {
+		return (WebElement) driver.findElements(By.id("search"));
+	}
+
+	//Method wait dialog for create and edit
+	public void waitForEditAndCreateDialog() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'dlgNewEditItem')]")));
+
+	}
+
+	//Method wait dialog for delete
+	public void waitForDeleteDialog() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'v-dialog')]\")[7]")));
+	}
+	
+	
+	//Save button from dialog edit and create	
+	public WebElement getSaveButton() {
+		return (WebElement) driver.findElements(By.xpath("//button[contains(@class,'btnSave')]"));
+	}
+	
+	//Delete button from delete dialog
+	public WebElement getDeleteButton() {
+		return (WebElement) driver.findElements(By.xpath("//*[text()=' Delete ']"));
+	}
+
+	//	metodu koja ceka da se u tabeli pojavi odredjeni broj redova.
+	public void waitForRowsNumberToBeVisible(int rowNum) {
+		wait.until(ExpectedConditions.numberOfElementsToBe(By.xpath("//tbody/tr"), rowNum));
+	}
+
+	 public WebElement getTableCell(int row, int cell) {
+	        return driver.findElement(By.xpath("//table//tr" + "[" + row + "]" + "/td" + "[" + cell + "]"));
+	    }
+
+	public WebElement getRowEditButton(int row) {
+        return driver.findElement(By.xpath("//table//tr" + "[" + row + "]" + "//button[1]"));
+    }
+
+    public WebElement getRowDeleteButton(int row) {
+        return driver.findElement(By.xpath("//table//tr" + "[" + row + "]" + "//button[2]"));
+    }
+	
 }
--- src/pages/MessagePopUpPage.java
+++ src/pages/MessagePopUpPage.java
@@ -1,5 +1,72 @@
 package pages;
 
-public class MassagePopUpPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class MessagePopUpPage {
+//	metodu koja ceka da pop up bude vidljiv
+//	Elemente koji u sebi sadrze tekstove poruke
+//	Close dugme
+//	metodu koja ceka da se verify your account dijalog pojavi
+//	metodu koja vraca zaglavlje iz verify your account dijaloga 
+//	koji sadrzi tekst IMPORTANT: Verify your account
+//	Close dugme iz verify account dijaloga
+	
+	
+	
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	public MessagePopUpPage(WebDriver driver, WebDriverWait wait) {
+		this.driver = driver;
+		this.wait = wait;
+	}
+	
+	//Wait popup to be visible
+	public void waitForPopUp() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'v-snack__content')]")));
+	}
+	
+	//Popup elements with massage
+	public WebElement getPopUpMessage() {
+		return (WebElement) driver.findElements(By.xpath("//div[contains(@class,'v-snack__content')]/ul/li"));
+		
+	}
+	
+	//Close btn
+	public WebElement getCloseBtn() {
+		return (WebElement) driver.findElements(By.xpath("//button[contains(@class,'v-btn')][4]"));
+	}
+	
+	//Wait verify account dlg
+	public void waitVerifyAccountDialog() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'v-dialog--active')]")));
+	}
+	
+	//Verify your account text
+	public String getTextFromVerifyAccount() {
+		return driver.findElement(By.xpath("//div[contains(@class, 'dlgVerifyAccount')]")).getText();
+	    }
+	
+	//Close dugme iz verify account dijaloga	
+	public WebElement getVerifyCloseButton() {
+		return driver.findElement(By.xpath("//button[contains(@class, 'btnClose')]"));
+		}
+	
+	
+	public WebElement successDialog() {
+        return driver.findElement(By.xpath("//div[contains(@class, 'success')]"));
+    }
+	
+	
+		
+	
+	
+	
+
+	
+	
 }
